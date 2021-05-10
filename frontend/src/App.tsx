2c925293491a23ce7ebe97fc097715ea300cdd49
alexandreluis <alexandreluisrigotti@gmail.com>
2021-05-10 00:53:04 -0300

    Atualizado arquivo com correção: gráficos ficam dispostos na mesma linha

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,16 +16,14 @@ function App() {
             <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
             <BarChart />
           </div>
-        </div>
 
-        <div className="row px-3">
           <div className="col-sm-6">
             <h5 className="text-center text-secondary">Total de vendas</h5>
             <DonutChart />
           </div>
         </div>
-
-        <div className = "py-3">
+        
+        <div className="py-3">
           <h2 className="text-primary">Todas vendas</h2>
         </div>
 
